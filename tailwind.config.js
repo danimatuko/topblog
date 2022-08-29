b4628921e680e060392ffebbd717484caacaafd1
Dani Matuko <matuko305@gmail.com>
2022-08-29 16:20:18 +0300

    add basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ec4899',
+      },
+    },
   },
   plugins: [],
 };
